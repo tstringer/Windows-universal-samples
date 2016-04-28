d777e09c88a37879809af7580bd3ab77d94ee056
Thomas Stringer <tr.stringer@outlook.com>
2016-04-28 15:29:36 -0400

    Created language change issue repro for media transport controls

--- Samples/XamlCustomMediaTransportControls/cs/MainPage.xaml.cs
+++ Samples/XamlCustomMediaTransportControls/cs/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Globalization;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,7 +28,23 @@ namespace CustomMediaTransportControls
             this.InitializeComponent();
 
             //This following line is setting the source for the MediaElement
-            this.MainME.Source = new System.Uri("http://go.microsoft.com/fwlink/p/?LinkID=272585");
+            this.MainME.Source = new System.Uri("https://sec.ch9.ms/ch9/ad03/33a90710-0d66-4c48-8f7f-db974771ad03/WSFLArchitectureDeepuThomas_high.mp4");
+        }
+
+        private void ChangeToEnglish_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.CacheSize = 0;
+            ApplicationLanguages.PrimaryLanguageOverride = "en";
+            //MainME.TransportControls.Language = "en";
+            Frame.Navigate(this.GetType());
+        }
+
+        private void ChangeToSpanish_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.CacheSize = 0;
+            ApplicationLanguages.PrimaryLanguageOverride = "es";
+            //MainME.TransportControls.Language = "es";
+            Frame.Navigate(this.GetType());
         }
     }
 }
